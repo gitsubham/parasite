62713c0c2945e8e5c1b97c1a9681abe6b78aa224
Neeraj-nitd <neeraj.nayal.2008@gmail.com>
2015-09-26 20:18:59 +0530

    before db into

---
+++ src/main/java/org/nasscom/parasite/controller/DocumentDetailServlet.java
@@ -0,0 +1,28 @@
+package org.nasscom.parasite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DocumentDetailServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		doPost(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws IOException {
+
+	
+	
+	
+	
+	}
+
+
+}
